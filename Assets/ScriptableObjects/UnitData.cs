82acf66bcb7f69fbec10f78d577d60ee3b13478e
Aleksandr <shumaherr@gmail.com>
2021-08-20 17:50:54 +0200

    UnitsSO

---
+++ Assets/ScriptableObjects/UnitData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitType
+{
+    Swordman,
+    Spearman,
+    Horseman
+};
+[CreateAssetMenu(menuName = "Create Unit", fileName = "New Unit")]
+public class UnitData : ScriptableObject
+{
+    [SerializeField] private UnitType _unitType;
+    [SerializeField] private UnitType _killUnit;
+    [SerializeField] private byte _moves;
+    [SerializeField] private byte _damage;
+
+    [SerializeField] private GameObject _unitPrefab;
+    
+
+}
